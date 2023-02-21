dfc7e4d9d2aa6546295456ca170cdc07f46e0910
Souleymane DIALLO <s.diallo.souleymane.d@gmail.com>
2023-02-21 21:47:58 +0100

    feat: initiliaze docker compose for database postgresql

---
+++ migrations/1_roles.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS roles (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    description VARCHAR NULL
+);
---
+++ migrations/2_shops.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS shops (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    service VARCHAR NOT NULL
+);
